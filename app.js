f2b41da2a1b5d1d7784ba23f4ce5239a110ae801
BugDiver <vinaysh@thoughtworks.com>
2016-02-26 13:59:53 +0530

    [Vinay]:- added facebook authentication .
    What changes have you made?
    
    * added a bunch of files and routes to aouthinticate a user and create
    * a private room. still it's not creating the room bt it showing the url
    * according to user name.
    
    Why do we need this change?
    
    * to make a room for user.
    
    What can this change impact?
    
    * app can  aouthinticate a user through facebook.`

---
+++ app.js
@@ -0,0 +1,26 @@
+var passport = require('passport');
+var Strategy = require('passport-facebook').Strategy;
+var express = require('express');
+var app = express();
+
+var port = process.env.PORT || 8080;
+
+var io = require('socket.io').listen(app.listen(port));
+
+app.set('view engine', 'html');
+app.engine('html', require('ejs').renderFile);
+app.set('views', __dirname + '/views');
+app.use(express.static(__dirname + '/public'));
+app.use(require('morgan')('combined'));
+app.use(require('cookie-parser')());
+app.use(require('body-parser').urlencoded({ extended: true }));
+app.use(require('express-session')({ secret: 'keyboard cat', resave: true, saveUninitialized: true }));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+
+
+
+
+require('./lib/routes')(app, io ,passport ,Strategy);
